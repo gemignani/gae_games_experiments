daf38b6341d1a97211e6356d9f0983e0835c67f6
douglas.lima <douglas.lima@dlima14-pc.LGE.NET>
2014-04-16 17:34:09 -0300

    New stuff

--- game2/phaser_plugin_pathfinding-master/src/PathFinderPlugin.js
+++ game2/phaser_plugin_pathfinding-master/src/PathFinderPlugin.js
@@ -38,8 +38,7 @@ Phaser.Plugin.PathFinderPlugin.prototype.setGrid = function (grid, walkables, it
         this._grid[i] = []
         for (var j = 0; j < grid[i].length; j++)
         {
-			if (grid[i][j]) this._grid[i][j] = grid[i][j].index
-			else this._grid[i][j] = 0;
+            this._grid[i][j] = grid[i][j].index
         }
     }
     this._walkables = walkables;
--- game2/starstruck.js
+++ game2/starstruck.js
@@ -1,9 +1,10 @@
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'chaser', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
-    game.load.tilemap('level1', 'assets/games/starstruck/level1.json', null, Phaser.Tilemap.TILED_JSON);
+    //game.load.tilemap('level1', 'assets/games/starstruck/level1.json', null, Phaser.Tilemap.TILED_JSON);
+	game.load.tilemap('level0', 'assets/games/starstruck/level0.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles-1', 'assets/games/starstruck/tiles-1.png');
     game.load.spritesheet('dude', 'assets/games/starstruck/dude.png', 32, 48);
     game.load.spritesheet('blackdude', 'assets/games/starstruck/blackdude.png', 32, 48);
@@ -26,6 +27,11 @@ var jumpButton;
 var bg;
 var playerdoublejump = 0;
 var pathfinder;
+var under_calc = false;
+
+
+var marker;
+
 
 function create() {
 
@@ -33,31 +39,35 @@ function create() {
 
     game.stage.backgroundColor = '#000000';
 
-    //setup map
+    //set-up map
     bg = game.add.tileSprite(0, 0, 800, 600, 'background');
     bg.fixedToCamera = true;
-
-    map = game.add.tilemap('level1');
-    map.addTilesetImage('tiles-1');
-    map.setCollisionByExclusion([ 13, 14, 15, 16, 46, 47, 48, 49, 50, 51 ]);
+	
+	//tile-map
+    map = game.add.tilemap('level0');
+	map.addTilesetImage('tiles-1');
+	
+	//collision
+	map.setCollisionByExclusion([ 13, 14, 15, 16, 46, 47, 48, 49, 50, 51 ]);
     layer = map.createLayer('Tile Layer 1');
 
-    var walkables = [200];
+	//path-finding
+    var walkables = [0];
     pathfinder = game.plugins.add(Phaser.Plugin.PathFinderPlugin);
     pathfinder.setGrid(map.layers[0].data, walkables);
 
-    //  Un-comment this on to see the collision tiles
+    //Un-comment this on to see the collision tiles
     //layer.debug = true;
 
     //setup world
     layer.resizeWorld();
     game.physics.arcade.gravity.y = 350;
-
+	
     exit = game.add.sprite(500, 500, 'door');
 
-
     //adding main char 'player'
-    player = game.add.sprite(32, 32, 'dude');
+    //player = game.add.sprite(32, 32, 'dude');
+	player = game.add.sprite(32, 900, 'dude');
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
     player.body.bounce.y = 0.2;
@@ -71,7 +81,8 @@ function create() {
     game.camera.follow(player);
 
    //adding enemy char 'enemy'
-    enemy = game.add.sprite(62, 32, 'blackdude');
+    //enemy = game.add.sprite(62, 32, 'blackdude');
+	enemy = game.add.sprite(62, 900, 'blackdude');
     game.physics.enable(enemy, Phaser.Physics.ARCADE);
 
     enemy.body.bounce.y = 0.2;
@@ -83,6 +94,13 @@ function create() {
     enemy.animations.add('right', [5, 6, 7, 8], 10, true);
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+	
+	
+	
+    marker = game.add.graphics();
+    marker.lineStyle(2, 0x000000, 1);
+    marker.drawRect(0, 0, 32, 32);
+	
 
 }
 
@@ -90,13 +108,19 @@ function findPathTo(tilex, tiley) {
 
     pathfinder.setCallbackFunction(function(path) {
         path = path || [];
+		if (path==null) {
+			alert("Path was not found.");
+		} else {
+			alert("Path was found. The first Point is " + path[0].x + " " + path[0].y);
+		}
         for(var i = 0, ilen = path.length; i < ilen; i++) {
             map.putTile(46, path[i].x, path[i].y);
         }
-        blocked = false;
+        //under_calc = false;
     });
-
-    pathfinder.preparePathCalculation([0,0], [tilex,tiley]);
+//enemy.position.x, enemy.position.y
+    //pathfinder.preparePathCalculation([0, 0], [30, 30]);
+	pathfinder.preparePathCalculation([0,0], [tilex,tiley]);
     pathfinder.calculatePath();
 }
 
@@ -150,24 +174,30 @@ function update() {
     
     if (jumpButton.isDown && game.time.now > jumpTimer)
     {
-	if (player.body.onFloor() == 1 ) {
-		playerdoublejump = 0;
-        	player.body.velocity.y = -250;
-        	jumpTimer = game.time.now + 550;
-	} else if (playerdoublejump == 0) {
-	       	player.body.velocity.y = -200;
-        	jumpTimer = game.time.now + 500;
-		playerdoublejump++;
-	}
-	 
+		if (player.body.onFloor() == 1 ) {
+			playerdoublejump = 0;
+				player.body.velocity.y = -250;
+				jumpTimer = game.time.now + 550;
+		} else if (playerdoublejump == 0) {
+				player.body.velocity.y = -200;
+				jumpTimer = game.time.now + 500;
+			playerdoublejump++;
+		}
     }
-/*
-    if (game.input.mousePointer.isDown)
+
+	marker.x = layer.getTileX(game.input.activePointer.worldX);
+    marker.y = layer.getTileY(game.input.activePointer.worldY);
+
+	
+    if (game.input.mousePointer.isDown && !under_calc)
     {
-        blocked = true;
-        findPathTo(layer.getTileX(marker.x), layer.getTileY(marker.y));
+		under_calc = true;
+		alert("pointer marker.x = " + marker.x + " pointer marker.y = " + marker.y);		
+		alert("layer.getTileX(marker.x) = " + layer.getTileX(marker.x) + " layer.getTileY(marker.y) = " + layer.getTileY(marker.y));		
+        //findPathTo(exit.x, exit.y);
+		findPathTo(layer.getTileX(marker.x), layer.getTileY(marker.y));
     }
-*/
+
 
 }
 
